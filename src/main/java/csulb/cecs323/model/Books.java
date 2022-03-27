2eeb20f5bf8422e67e36491b53e982ce9ff2d211
Grant Smith <59898052+grantsmith01@users.noreply.github.com>
2022-03-27 12:54:38 -0700

    - Added queries for returning books by title and authoring entity name, returning all books, and updating books. - Added publisher and authoringEntity columns to Book class, added these attributes to the Constructor, added them to the toString, and added their getters and setters (since Book inherits from Publisher and AuthoringEntity) - Added requirement 5b, a getter for the Book's PK (title and ISBN)

--- src/main/java/csulb/cecs323/model/Books.java
+++ src/main/java/csulb/cecs323/model/Books.java
@@ -1,16 +1,38 @@
 package csulb.cecs323.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedNativeQuery;
+import javax.persistence.*;
 
 @Entity
 @NamedNativeQuery(
         name="ReturnBook",
         query = "SELECT * " +
-                "FROM   BOOKS " +
+                "FROM   books " +
                 "WHERE  ISBN = ? ",
+        resultClass = Books.class
+)
+
+@NamedNativeQuery(
+        name="ReturnBookTitleAuthoringEntityName",
+        query = "SELECT * " +
+                "FROM books " +
+                "WHERE title = ? AND authoring_entity_name = ?",
+        resultClass = Books.class
+)
+
+@NamedNativeQuery(
+        name="ReturnAllBooks",
+        query = "SELECT * " +
+                "FROM books ",
+        resultClass = Books.class
+)
+/**
+ * 4. Update a Book – Change the authoring entity for an existing book.
+ */
+@NamedNativeQuery(
+        name="UpdateBook",
+        query = "UPDATE books " +
+                "SET authoring_entity_name = ? " +
+                "WHERE ISBN = ?",
         resultClass = Books.class
 )
 
@@ -31,6 +53,18 @@ public class Books {
     /** The year the book was published. */
     private int yearPublished;
 
+    @ManyToOne
+    @JoinColumn(name = "publisher_name",
+            referencedColumnName = "name")
+    /**  The publishing company of the book. **/
+    private Publishers publisher;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_entity_name",
+            referencedColumnName = "name")
+    /**  The authoring entity of the book. **/
+    private AuthoringEntities authoringEntity;
+
     /** Default constructor that creates a Book. */
     public Books() {}
 
@@ -39,10 +73,12 @@ public class Books {
      *  @param title             The title of the book.
      *  @param yearPublished     The year the book was published.
      */
-    public Books (String ISBN, String title, int yearPublished) {
+    public Books (String ISBN, String title, int yearPublished, Publishers publisher, AuthoringEntities authoringEntity) {
         this.ISBN = ISBN;
         this.title = title;
         this.yearPublished = yearPublished;
+        this.publisher = publisher;
+        this.authoringEntity = authoringEntity;
     }
 
     /** Retrieves the ISBN of the book.
@@ -87,12 +123,41 @@ public class Books {
         this.yearPublished = yearPublished;
     }
 
+    /** Retrieves the publisher of the book.
+     * @return publisher  The publisher of the book.
+     */
+    public Publishers getPublisher() { return publisher; }
+
+    /** Sets the publisher of the book.
+     * @param publisher   The publisher of the book.
+     */
+    public void setPublisher(Publishers publisher) { this.publisher = publisher; }
+
+    /**
+     * Retrieves the authoring entity of the book.
+     * @return authoringEntity      The authoring entity of the book.
+     */
+    public AuthoringEntities getAuthoringEntity() { return authoringEntity; }
+
+    /**
+     * Sets the authoring entity of the book.
+     * @param authoringEntity      The authoring entity of the book.
+     */
+    public void setAuthoringEntity(AuthoringEntities authoringEntity) { this.authoringEntity = authoringEntity; }
+
+    /** 5b. List the primary key of all the rows of Books (show the title as well as the ISBN)
+     * Lists the Primary Key of the Book, which is the title and the ISBN, as a String
+     * @return String of the PK      The title and ISBN of the book as a String.
+     */
+    public String getBookPK() { return "Book title: " + title + ", ISBN: " + ISBN; }
+
     /** The string representation of Books
      * @return string representation of Books
      */
     @Override
     public String toString () {
         return "Book\nISBN: " + this.ISBN+ " Title: " + this.title +
-                " Year Published: " + this.yearPublished;
+                " Year Published: " + this.yearPublished + " Publisher: " + this.publisher +
+                " Authoring Entity: " + this.authoringEntity;
     }
 }
