2c6409d4363fce21b15a6dbedfc92097764f0636
anarcopossum <92814289+anarcopossum@users.noreply.github.com>
2022-04-01 19:29:07 -0700

    WritingGroup Functions implemented
    I added the Writing group functions, please check it out and do any changes as needed. also needs comments.

--- src/main/java/csulb/cecs323/model/WritingGroups.java
+++ src/main/java/csulb/cecs323/model/WritingGroups.java
@@ -15,4 +15,30 @@ import javax.persistence.NamedNativeQuery;
 )
 
 public class WritingGroups {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String head_writer;
+
+    @Column(nullable = false)
+    private int year_formed;
+
+    @Column(nullable = true, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    public WritingGroups(){}
+
+    public String getHead_writer() {return head_writer;}
+    public void setHead_writer(String Head_writer) {this.head_writer = head_writer;}
+
+    public int getyear_formed() {return year_formed;}
+    public void setyear_formed(int year_formed) {this.year_formed = year_formed;}
+
+    public String getname() {return name;}
+    public void setname(String name) {this.name = name;}
+
+    public String getemail() {return email;}
+    public void setemail(String email) {this.email = email;}
 }
