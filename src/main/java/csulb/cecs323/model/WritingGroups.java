15b424f30f157e8481d3231f2478882da3c9c9d7
Grant Smith <59898052+grantsmith01@users.noreply.github.com>
2022-04-02 22:13:44 -0700

    - Added a query to get the writing group from authoringEntities using email and authoring entity type - Specified discriminator value - extended authoring entities - overrided getType function to return the type in string form - added toString - added constructor

--- src/main/java/csulb/cecs323/model/WritingGroups.java
+++ src/main/java/csulb/cecs323/model/WritingGroups.java
@@ -1,44 +1,79 @@
+//Good as of 4/2
 package csulb.cecs323.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedNativeQuery;
+import javax.persistence.DiscriminatorValue;
 
 @Entity
 @NamedNativeQuery(
-        name="ReturnWritingGroups",
+        name="ReturnWritingGroup",
         query = "SELECT * " +
-                "FROM   WritingGroups " +
-                "WHERE  name = ? ",
+                "FROM   authoringEntities " +
+                "WHERE  email = ? AND authoringEntityType = ?",
         resultClass = WritingGroups.class
 )
 
-public class WritingGroups {
-    @Id
+@DiscriminatorValue("Writing Groups")
+public class WritingGroups extends AuthoringEntities {
     @Column(nullable = false, length = 20)
-    private String head_writer;
+    private String headWriter;
 
-    @Column(nullable = false)
-    private int year_formed;
+    @Column(nullable = false, length = 4)
+    private int yearFormed;
 
-    @Column(nullable = true, length = 20)
-    private String name;
+    /** Returns this specific authoring entity type as "Writing Group"
+     * @return "Writing Group"  The authoring entity type of this authoring entity in String form
+     */
+    @Override
+    public String getType() { return "Writing Group"; }
 
-    @Column(nullable = false, length = 30)
-    private String email;
+    /**
+     * Calls default constructor of parent class for this child class's default constructor
+     */
+    public WritingGroups(){ super(); }
 
-    public WritingGroups(){}
+    /** Creates a WritingGroups object with the email, name, headWriter, and yearFormed specified.
+     * @param email             The email for the writing group.
+     * @param name              The name of the writing group.
+     * @param headWriter        The head writer for the writing group.
+     * @param yearFormed        The year the writing group was formed.
+     */
+    public WritingGroups(String email, String name, String headWriter, int yearFormed)
+    {
+        super(email, name);  //calls parent's constructor for email and name, assigns email and name
+        this.headWriter = headWriter;
+        this.yearFormed = yearFormed;
+    }
 
-    public String getHead_writer() {return head_writer;}
-    public void setHead_writer(String Head_writer) {this.head_writer = head_writer;}
+    /** Retrieves the head writer of the writing group.
+     * @return headWriter       The head writer of the writing group.
+     */
+    public String getHeadWriter() {return headWriter;}
 
-    public int getyear_formed() {return year_formed;}
-    public void setyear_formed(int year_formed) {this.year_formed = year_formed;}
+    /** Sets the head writer of the writing group to the head writer specified.
+     * @param HeadWriter       The head writer to set for the writing group.
+     */
+    public void setHeadWriter(String HeadWriter) {this.headWriter = headWriter;}
 
-    public String getname() {return name;}
-    public void setname(String name) {this.name = name;}
+    /** Retrieves the year formed of the writing group.
+     * @return yearFormed       The year formed of the writing group.
+     */
+    public int getYearFormed() {return yearFormed;}
 
-    public String getemail() {return email;}
-    public void setemail(String email) {this.email = email;}
+    /** Sets the year formed of the writing group to the year formed specified.
+     * @param yearFormed       The year formed to set for the writing group.
+     */
+    public void setYearFormed(int yearFormed) {this.yearFormed = yearFormed;}
+
+    /** Outputs this class in a string representation.
+     * @return the class in a string representation
+     */
+    public String toString()
+    {
+        return "Writing Group\nName: " + super.getName() + ", eMail: " + super.getEmail() + ", Head writer: " +
+                this.headWriter + "Year formed: " + this.yearFormed;
+    }
 }
