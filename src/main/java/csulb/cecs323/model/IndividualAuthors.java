986fe617f7d4edb707ab2edf0f3d38d1c511525f
Grant Smith <59898052+grantsmith01@users.noreply.github.com>
2022-04-02 23:01:19 -0700

    - Updated Individual Authors similar to WritingGroups - IndividualAuthors needs a Many to Many join table between it and AdHocTeams

--- src/main/java/csulb/cecs323/model/IndividualAuthors.java
+++ src/main/java/csulb/cecs323/model/IndividualAuthors.java
@@ -1,30 +1,24 @@
+//Good as of 4/2
 package csulb.cecs323.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedNativeQuery;
+import java.util.List;
+import java.util.ArrayList;
+import javax.persistence.*;
 
 @Entity
-@NamedNativeQuery(
-        name="ReturnIndividualAuthors",
-        query = "SELECT * " +
-                "FROM   authors " +
-                "WHERE  name = ? ",
-        resultClass = IndividualAuthors.class
-)
+@DiscriminatorValue("Individual Authors")
+public class IndividualAuthors extends AuthoringEntities {
 
-public class IndividualAuthors {
-    @Id
-    @Column(nullable = false, length = 20)
-    /** The name of the Author. Limited to 20 characters. */
-    private String name;
+    @ManyToMany
+    @JoinTable(
+            name = "MemberAdHocTeams"
+    )
+    List<AdHocTeams> adHocTeamsList;
 
-    @Column(nullable = false, length = 30)
-    /** The email of the Author.  Limited to 30 characters. */
-    private String email;
-
-    public IndividualAuthors() {}
+    /**
+     * Calls default constructor of parent class for this child class's default constructor
+     */
+    public IndividualAuthors() { super(); }
 
     /**
      * Overloaded constructor that creates a IndividualAuthors object.
@@ -32,50 +26,30 @@ public class IndividualAuthors {
      * @param name  The Name of the Author
      * @param email The Email of the Author
      */
-    public IndividualAuthors(String name, String email) {
-        this.name = name;
-        this.email = email;
+    public IndividualAuthors(String email, String name) {
+        super(email, name);
+        adHocTeamsList = new ArrayList<>();
     }
 
-    /**
-     * Retrieves the name of the Author
-     *
-     * @return name  the name of the Author
+    /** Outputs this class in a string representation.
+     * @return the class in a string representation
      */
-    public String getName() {
-        return name;
-    }
+    public String toString() { return "Individual Author:\nName: " + super.getName() + " Email: " + super.getEmail(); }
 
-    /**
-     * Sets the name for the Author
-     *
-     * @param name the name of the Author
+    /** Retrieves the list of ad hoc teams this individual author belongs to.
+     * @return adHocTeamsList   The list of ad hoc teams this individual author belongs to.
      */
-    public void setName(String name) {
-        this.name = name;
-    }
+    public List<AdHocTeams> getAdHocTeams() { return this.adHocTeamsList; }
 
-    /**
-     * Retrieves the email of the Author
-     *
-     * @return email the email of the author
+    /** Adds an ad hoc team to this individual author's list of ad hoc teams.
+     * @param adHocTeam     The ad hoc team to add to the list of ad hoc teams.
      */
-    public String getEmail() {
-        return email;
-    }
+    public void addAdHocTeam(AdHocTeams adHocTeam) { this.adHocTeamsList.add(adHocTeam); };
 
-    /**
-     * Sets the name for the Author
-     *
-     * @param email The email of the other
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /** The string representation of Individual Authors
-     * @return string representation of Individual Authors
+    /** Returns this specific authoring entity type as "Individual Author"
+     * @return "Individual Author"  The authoring entity type of this authoring entity in String form
      */
     @Override
-    public String toString() {return "Publisher\nName: " + this.name + " Email: " + this.email;}
+    public String getType() { return "Individual Author"; }
+
 }
