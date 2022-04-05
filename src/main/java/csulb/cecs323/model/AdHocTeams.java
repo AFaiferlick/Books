666794bd6f47dc4f413cf005ea9de802dda6697f
Grant Smith <59898052+grantsmith01@users.noreply.github.com>
2022-04-04 19:25:40 -0700

    Removed mappedBy = "adHocTeamsList", which caused errors

--- src/main/java/csulb/cecs323/model/AdHocTeams.java
+++ src/main/java/csulb/cecs323/model/AdHocTeams.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @DiscriminatorValue("Ad Hoc Teams")
 public class AdHocTeams extends AuthoringEntities {
 
-    @ManyToMany(mappedBy = "adHocTeamsList")
+    @ManyToMany//(mappedBy = "adHocTeamsList")
     List<IndividualAuthors> individualAuthorsList;
 
     /**
